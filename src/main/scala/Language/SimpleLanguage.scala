a666bf945783a5e2daca665d512dfd9cc8ff0ad2
Michael Zhao <michaelzhao007@hotmail.com>
2017-12-31 19:10:58 -0500

    initial commit for a simple language compiler written in scala

--- src/main/scala/Language/SimpleLanguage.scala
+++ src/main/scala/Language/SimpleLanguage.scala
@@ -11,15 +11,16 @@ object SimpleLanguage extends JavaTokenParsers with PackratParsers with Implicit
   private val separator = eoi | eol
 
   sealed trait Expr
-  case class BinOpExpr(op: String, left: Expr, right: Expr) extends Expr
+  case class BinOpExpr(op: String, left: String, right: String) extends Expr
   case class Num(v: Int) extends Expr
   case class Variable(name: String, f: Num) extends Expr
   case class Function(args: Int, vars: List[Num]) extends Expr
   case class Loop(repeat: Expr, exp: Expr) extends Expr
   case class Computation(id: String, v: List[Num]) extends Expr
-  case class Assign(name: String, v: Expr) extends Expr
+  case class Assign(binOp: BinOpExpr, v3: String) extends Expr
+  case class While(times: Int, assign: Assign) extends Expr
 
-  val vmap = new mutable.HashMap[String, Expr]()
+  val vmap = new mutable.HashMap[String, Num]()
   val fmap = new mutable.HashMap[String, Function]()
 
   def numericLit = """\d+(\.\d*)?""".r map { elem => Num(elem.toInt) }
@@ -58,13 +59,45 @@ object SimpleLanguage extends JavaTokenParsers with PackratParsers with Implicit
 
   def assign = assignFuncWithID | assignNumWithID | assignVarWithID
 
+  def loop = ("do" ~ "{") ~> numericLit ~ "}" ~ "assign" ~ identifier ~ ("*"|"-"|"/"|"+") ~ identifier ~ ("to") ~ identifier ^^ {
+    case numloop ~ "}" ~ "assign" ~ v1 ~ op ~ v2 ~ "to" ~ v3 => {
+      While(numloop.v, Assign(BinOpExpr(op, v1, v2), v3))
+    }
+  }
+
   def compute = ("what" ~ "is") ~> identifier ~ opt(rep("[" ~> numericLit <~ "]")) ^^ {
     case valname ~ None => Computation(valname, Nil)
     case valname ~ Some(f) => Computation(valname, f)
   }
 
-  def eval(t: Object): Any = t match {
+  def evalBinOpExpr(binOp: BinOpExpr): Num  = binOp match {
+    case BinOpExpr(op, l, r) => {
+      (op, vmap.get(l), vmap.get(r)) match {
+        case ("*", Some(ll), Some(rr)) => Num(ll.v * rr.v)
+        case ("-", Some(ll), Some(rr)) => Num(ll.v - rr.v)
+        case ("/", Some(ll), Some(rr)) => Num(ll.v / rr.v)
+        case ("+", Some(ll), Some(rr)) => Num(ll.v + rr.v)
+      }
+    }
+  }
+
+  def eval(t: Object): Expr = t match {
     case Num(v) => Num(v)
+    case binOp: BinOpExpr => evalBinOpExpr(binOp)
+    case Assign(binOp, v) => {
+       val res = evalBinOpExpr(binOp)
+       vmap.put(v, res)
+       res
+    }
+    case While(numloop, assignStmt) => {
+      for(i <- 0 until numloop) {
+        eval(assignStmt)
+      }
+      vmap.get(assignStmt.v3) match {
+        case None => new ParseException("variable is not associated with any existing function")
+        case Some(v) => v
+      }
+    }
     case Computation(name, Nil) => {
         val f = vmap.get(name)
         f match {
@@ -82,9 +115,9 @@ object SimpleLanguage extends JavaTokenParsers with PackratParsers with Implicit
           } yield v.vars(i).v * arl(i).v).foldLeft(0){_ + _}
           val rem  = v.vars.drop(arl.size)
           if(rem.size <= 1) {
-            val variable = Variable(name, Num(compRes + rem.head.v))
-            vmap.put(name, variable)
-            variable
+            val res = Num(compRes + rem.head.v)
+            vmap.put(name, res)
+            res
           }
           else {
             val newfs = rem ++ List(Num(compRes))
@@ -95,10 +128,11 @@ object SimpleLanguage extends JavaTokenParsers with PackratParsers with Implicit
         }
       }
     }
-    case v => v
+    case v: Variable => v
+    case f: Function => f
   }
 
-  def lang = compute | assign
+  def lang = compute | assign | loop
 
   def parseLang = phrase(lang)
 
@@ -118,7 +152,13 @@ object SimpleLanguage extends JavaTokenParsers with PackratParsers with Implicit
 }
 
 object SimpleLanguageTest extends App {
-  //
-  //
-  SimpleLanguage.parse("ab is 10;what is ab;cd is function of 2:2,3,5;what is cd[7][2];")
+  SimpleLanguage.parse(
+    "I is 2;" +
+    "F1 is 2;" +
+    "do {2} assign I + F1 to F1;" +
+    "what is F1;" +
+    "ab is 10;" +
+    "what is ab;" +
+    "cd is function of 2:2,3,5;" +
+    "what is cd[7][2];")
 }
